b750b04c47acf56e1a57dc4713ad569e58f0086b
sescie <94220701+sescie@users.noreply.github.com>
2023-09-26 09:31:54 +0200

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -17,7 +17,7 @@ R204440W
 #Imports
 import streamlit as st
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import *
 import numpy as np
 import pickle
 import keras_preprocessing
